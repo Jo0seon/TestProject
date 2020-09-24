50eaa96230b0ad6b24e48bf629a904bc84b20ed8
ntomsky <nikita.tomsky@gmail.com>
2020-09-23 22:12:58 -0400

    second try

--- src/TestDrive.java
+++ src/TestDrive.java
@@ -1,7 +1,11 @@
 public class TestDrive {
     //version 1
-    
+    public static void main(String[] args){
+        int a = 0;
+    }
     //edit by Nik
     
     //changes from a branch
+
+    //commit from Intellij by NT attempt#1
 }
